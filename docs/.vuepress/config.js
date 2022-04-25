7944f4c55434053a5a12868a7bd41d6af5d30501
Vingkin <1830053226@qq.com>
2022-04-25 15:19:40 +0800

    update docs

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -15,6 +15,7 @@ module.exports = {
     themeConfig: {
         // lastUpdated: '上次更新',
         subSidebar: 'auto',
+        logo: 'logo.jpg',
         nav: [
             { text: '首页', link: '/' },
             {
@@ -65,8 +66,8 @@ module.exports = {
                             title: '计算机基础',
                             children: [
                                 { title: '操作系统', path: '/guide/interview/OS' },
-                                { title: '计算机网络'},
-                                { title: '数据结构', path: '/guide/interview/DataStructure'},
+                                { title: '计算机网络' },
+                                { title: '数据结构', path: '/guide/interview/DataStructure' },
                                 { title: '设计模式', path: '/guide/interview/DesignMode' },
                                 { title: '代码模板', path: '/guide/interview/CodeTemplate' }
                             ]
@@ -84,7 +85,36 @@ module.exports = {
                 buttonText: '更新'
             }
         },
+        // 假定是 GitHub. 同时也可以是一个完整的 GitLab URL
+        repo: 'vingkin/Blog',
+        // 自定义仓库链接文字。默认从 `themeConfig.repo` 中自动推断为
+        // "GitHub"/"GitLab"/"Bitbucket" 其中之一，或是 "Source"。
+        repoLabel: '查看源码',
+
+        // 以下为可选的编辑链接选项
+
+        // 假如你的文档仓库和项目本身不在一个仓库：
+        // docsRepo: 'vuejs/vuepress',
+        // 假如文档不是放在仓库的根目录下：
+        docsDir: 'docs/',
+        // 假如文档放在一个特定的分支下：
+        docsBranch: 'docs',
+        // 默认是 false, 设置为 true 来启用
         editLinks: true,
-        editLinkText: '在 GitHub 上编辑此页 ！'
-    }
+        // 默认为 "Edit this page"
+        editLinkText: '编辑此页'
+    },
+    plugins: [
+        [
+            'vuepress-plugin-code-copy', true
+        ]
+    ]
+    // plugins: [
+    //     ['vuepress-plugin-nuggets-style-copy', {
+    //         copyText: '复制',
+    //         tip: {
+    //             content: '复制成功'
+    //         }
+    //     }]
+    // ]
 }
