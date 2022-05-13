33636887852c6bd5003cc5f03ce9d1d6572c5ec6
Vingkin <1830053226@qq.com>
2022-05-13 19:21:19 +0800

    update docs

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     title: 'Vingkin',
     description: '我是要成为火影的男人',
-    base: '/Blog/',
+    // base: '/Blog/',
     theme: 'reco',
     icon: 'logo.jpg',
     timeline: true,
